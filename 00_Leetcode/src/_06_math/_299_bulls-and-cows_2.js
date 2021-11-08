9db30793230794b1d32e43b90c9a2007ffb8e2d9
angryreid <onyourwill@gmail.com>
2021-11-08 22:24:39 +0800

    反转单词两种方法实现

---
+++ 00_Leetcode/src/_06_math/_299_bulls-and-cows_2.js
@@ -0,0 +1,37 @@
+// "1123"
+// "0111"
+
+
+function getHint(secret, guess) {
+  let secretMap = new Map();
+  let guessMap = new Map();
+
+  let bulls = 0, cows = 0;
+  for (let i = 0; i < secret.length; i++) {
+    if (secret[i] == guess[i]) {
+      bulls++;
+    } else {
+      const secretKey = secret[i];
+      const guessKey = guess[i];
+      !secretMap.has(secretKey) && secretMap.set(secretKey, 0);
+      !guessMap.has(guessKey) && guessMap.set(guessKey, 0);
+      secretMap.set(secretKey, secretMap.get(secret) + 1);
+      guessMap.set(guessKey, guessMap.get(guessKey) + 1);
+    }
+  }
+
+  secretMap.forEach((count, key) => {
+    if (guessMap.has(key)) {
+      const guessCount = guessMap.get(key);
+      if (count <= guessCount) {
+        cows += count;
+      } else {
+        cows += guessCount;
+      }
+    }
+  })
+
+  return `${bulls}A${cows}B`;
+};
+
+console.log(getHint('1123', '0111'));
