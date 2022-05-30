ddc1571e22bbf99076d84b8c125c2346bb7a4688
angryreid <onyourwill@gmail.com>
2022-05-30 22:55:24 +0800

    Feature: Added Bubble sort test result.

--- 16_Instance/01_Sort/src/emma/Main.java
+++ 16_Instance/01_Sort/src/emma/Main.java
@@ -1,26 +1,50 @@
 package emma;
 
+import emma.tool.Integers;
+import emma.tool.Times;
+
+import java.sql.Time;
+
 public class Main {
     public static void main(String[] args) {
-        int[] list = {9, 3, 4, 2, 5, 6, 8, 88, 45, 2, 2, 2};
-        for (int i = list.length - 1; i > 0; i--) {
-            boolean sort = true;
-            for (int j = 1; j <= i; j++) {
-                if (list[j] < list[j - 1]) {
-                    int temp = list[j];
-                    list[j] = list[j - 1];
-                    list[j - 1] = temp;
-                    sort = false;
+        Integer[] list = Integers.random(20000, 1, 100000);
+        Integer[] list2 = Integers.copy(list);
+        Times.test("Bubble Sort 1", () -> {
+            for (int i = list.length - 1; i > 0; i--) {
+                boolean sort = true;
+                for (int j = 1; j <= i; j++) {
+                    if (list[j] < list[j - 1]) {
+                        int temp = list[j];
+                        list[j] = list[j - 1];
+                        list[j - 1] = temp;
+                        sort = false;
+                    }
+                }
+                if (sort) break;
+            }
+        });
+
+        Times.test("Bubble sort 2", () -> {
+            for (int i = 0; i < list2.length - 1; i++) {
+                for (int j = i + 1; j < list2.length; j++) {
+                    if (list2[i] > list2[j]) {
+                        int temp = list2[i];
+                        list2[i] = list2[j];
+                        list2[j] = temp;
+                    }
                 }
             }
-            System.out.println("hello");
-            if (sort) break;
-        }
+        });
 
-        // After sorted
-        for (int i = 0; i < list.length; i++) {
-            System.out.print(" " + list[i]);
-        }
-//        System.out.println("Sorting list: " + list.toString());
+//        [Bubble Sort 1]
+//        Start time: 22:54:06.692
+//        End time: 22:54:08.753
+//        Cost: 2.061s
+//        -------------------------------------
+//        [Bubble sort 2]
+//        Start time: 22:54:08.755
+//        End time: 22:54:09.827
+//        Cost: 1.072s
+//        -------------------------------------
     }
 }
--- 16_Instance/01_Sort/src/emma/tool/Times.java
+++ 16_Instance/01_Sort/src/emma/tool/Times.java
@@ -12,15 +12,15 @@ public class Times {
 	
 	public static void test(String title, Task task) {
 		if (task == null) return;
-		title = (title == null) ? "" : ("【" + title + "】");
+		title = (title == null) ? "" : ("[" + title + "]");
 		System.out.println(title);
-		System.out.println("开始：" + fmt.format(new Date()));
+		System.out.println("Start time: " + fmt.format(new Date()));
 		long begin = System.currentTimeMillis();
 		task.execute();
 		long end = System.currentTimeMillis();
-		System.out.println("结束：" + fmt.format(new Date()));
+		System.out.println("End time: " + fmt.format(new Date()));
 		double delta = (end - begin) / 1000.0;
-		System.out.println("耗时：" + delta + "秒");
+		System.out.println("Cost: " + delta + "s");
 		System.out.println("-------------------------------------");
 	}
 }
