3e6ecb5da7b2767f61863e4f692d47867d4b8445
angryreid <onyourwill@gmail.com>
2022-05-30 23:26:02 +0800

    Feature: Bubble test

--- 16_Instance/01_Sort/src/emma/Main.java
+++ 16_Instance/01_Sort/src/emma/Main.java
@@ -6,14 +6,20 @@ import emma.tool.Times;
 
 public class Main {
     public static void testBubble() {
-        Integer[] list = Integers.random(1000, 1, 100000);
+//        Integer[] list = Integers.random(10000, 1, 12000);
+        Integer[] list = Integers.tailAscOrder(1, 10000, 2000);
         Integer[] list2 = Integers.copy(list);
-        Times.test("Bubble Sort 1", () -> {
+        Integer[] list3 = Integers.copy(list);
+        Times.test("Bubble Sort Swap", () -> {
             Bubble.bubbleSwap(list);
         });
 
-        Times.test("Bubble sort 2", () -> {
+        Times.test("Bubble Sort Asc", () -> {
             Bubble.bubbleAsc(list2);
+        });
+
+        Times.test("Bubble Sort Sorted Index", () -> {
+            Bubble.bubbleSortedIndex(list3);
         });
     }
 
--- 16_Instance/01_Sort/src/emma/bubble/Bubble.java
+++ 16_Instance/01_Sort/src/emma/bubble/Bubble.java
@@ -17,6 +17,21 @@ public class Bubble {
         }
     }
 
+    public static void bubbleSortedIndex(Integer[] list) { // Applying for sorted number at the tail.
+        for (int i = list.length - 1; i > 0; i--) {
+            int sortedIndex = 1; // Sorted Array will use it.
+            for (int j = 1; j <= i; j++) {
+                if (list[j] < list[j - 1]) {
+                    int temp = list[j];
+                    list[j] = list[j - 1];
+                    list[j - 1] = temp;
+                    sortedIndex = j;
+                }
+            }
+            i = sortedIndex;
+        }
+    }
+
     public static void bubbleAsc(Integer[] list) {
         for (int i = 0; i < list.length - 1; i++) {
             for (int j = i + 1; j < list.length; j++) {
