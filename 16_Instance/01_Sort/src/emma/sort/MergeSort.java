4a62d96cbc35e783660a0ca9ffbae61d15edb77f
angryreid <onyourwill@gmail.com>
2022-06-09 23:20:14 +0800

    Feature: Merge Sort init

--- 16_Instance/01_Sort/src/emma/sort/MergeSort.java
+++ 16_Instance/01_Sort/src/emma/sort/MergeSort.java
@@ -3,6 +3,16 @@ package emma.sort;
 public class MergeSort<E extends Comparable<E>> extends Sort<E> {
     @Override
     protected void sort() {
+        sort(0, list.length);
+    }
+
+    /**
+     * sort at [start, end)
+     * @param start
+     * @param end
+     */
+    private void sort(int start, int end) {
+        if ((end - start) < 2) return;
         
     }
 }
