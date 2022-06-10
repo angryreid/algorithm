aa978d1ba1ed2257d8490210f323b8ed26643cf2
angryreid <onyourwill@gmail.com>
2022-06-10 22:50:45 +0800

    Feature: Merge Sort divid completed

--- 16_Instance/01_Sort/src/emma/sort/MergeSort.java
+++ 16_Instance/01_Sort/src/emma/sort/MergeSort.java
@@ -1,8 +1,10 @@
 package emma.sort;
 
 public class MergeSort<E extends Comparable<E>> extends Sort<E> {
+    private E[] leftArray;
     @Override
     protected void sort() {
+        leftArray = (E[]) new Object[list.length >> 1];
         sort(0, list.length);
     }
 
@@ -12,7 +14,20 @@ public class MergeSort<E extends Comparable<E>> extends Sort<E> {
      * @param end
      */
     private void sort(int start, int end) {
-        if ((end - start) < 2) return;
-        
+        if ((end - start) < 2) return; // only one element
+        int mid = (start + end) >> 1;
+        sort(start, mid);
+        sort(mid, end);
+        merge(start, mid, end);
+    }
+
+    /***
+     * Merge [start, mid), [mid, end)
+     * @param start
+     * @param mid
+     * @param end
+     */
+    private void merge(int start, int mid, int end) {
+
     }
 }
