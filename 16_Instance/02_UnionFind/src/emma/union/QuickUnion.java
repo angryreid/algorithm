3889091a6543bfdf39ecd6e9b7d6a0d2efdc3b19
angryreid <onyourwill@gmail.com>
2022-06-30 22:50:04 +0800

    F: Quick Union init.

---
+++ 16_Instance/02_UnionFind/src/emma/union/QuickUnion.java
@@ -0,0 +1,17 @@
+package emma.union;
+
+public class QuickUnion extends UnionFind {
+    public QuickUnion(int capacity) {
+        super(capacity);
+    }
+
+    @Override
+    public int find(int v) {
+        return 0;
+    }
+
+    @Override
+    public void union(int v1, int v2) {
+
+    }
+}
