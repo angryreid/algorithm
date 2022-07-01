6599b3356eb036a817f7bce04f29d4294cd82bbb
angryreid <onyourwill@gmail.com>
2022-07-01 22:25:13 +0800

    F: Quick Union v1 done.

--- 16_Instance/02_UnionFind/src/emma/Main.java
+++ 16_Instance/02_UnionFind/src/emma/Main.java
@@ -1,10 +1,13 @@
 package emma;
 
 import emma.union.QuickFind;
+import emma.union.QuickUnion;
+import emma.union.UnionFind;
 
 public class Main {
-    public static void main(String[] args) {
-        QuickFind uf = new QuickFind(12);
+
+    public static void testQuickFind() {
+        UnionFind uf = new QuickFind(12);
         uf.union(0, 1);
         uf.union(0, 3);
         uf.union(0, 4);
@@ -19,5 +22,27 @@ public class Main {
 
         System.out.println(uf.isSame(0, 5));
         System.out.println(uf.isSame(0, 6));
+    }
+
+    public static void testQuickUnion() {
+        UnionFind uf = new QuickUnion(12);
+        uf.union(0, 1);
+        uf.union(0, 3);
+        uf.union(0, 4);
+        uf.union(2, 3);
+        uf.union(2, 5);
+
+        uf.union(6, 7);
+
+        uf.union(8, 10);
+        uf.union(9, 10);
+        uf.union(9, 11);
+
+        System.out.println(uf.isSame(0, 5));
+        System.out.println(uf.isSame(0, 6));
+    }
+
+    public static void main(String[] args) {
+        testQuickUnion();
     }
 }
--- 16_Instance/02_UnionFind/src/emma/union/QuickUnion.java
+++ 16_Instance/02_UnionFind/src/emma/union/QuickUnion.java
@@ -7,11 +7,18 @@ public class QuickUnion extends UnionFind {
 
     @Override
     public int find(int v) {
-        return 0;
+        rangeCheck(v);
+        while (v != parents[v]) {
+            v = parents[v];
+        }
+        return v;
     }
 
     @Override
     public void union(int v1, int v2) {
-
+        int p1 = find(v1);
+        int p2 = find(v2);
+        if (p1 == p2) return;
+        parents[p1] = p2;
     }
 }
