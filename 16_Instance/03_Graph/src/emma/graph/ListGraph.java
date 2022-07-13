979918a61ac08b76b0f79c8d7d9247597bdc9ee7
angryreid <onyourwill@gmail.com>
2022-07-13 22:48:20 +0800

    F: Graph implementing

--- 16_Instance/03_Graph/src/emma/graph/ListGraph.java
+++ 16_Instance/03_Graph/src/emma/graph/ListGraph.java
@@ -1,12 +1,15 @@
 package emma.graph;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class ListGraph<V, E> implements Graph<V, E> {
+    private Map<V, Vertex<V, E>> vertices = new HashMap<>();
     @Override
     public int verticesSize() {
-        return 0;
+        return vertices.size();
     }
 
     @Override
@@ -16,11 +19,29 @@ public class ListGraph<V, E> implements Graph<V, E> {
 
     @Override
     public void addVertex(V v) {
+        if (vertices.containsKey(v)) return;
+        vertices.put(v, new Vertex<>(v));
+    }
 
+    @Override
+    public void addEdge(V from, V to) {
+        addEdge(from, to, null);
     }
 
     @Override
     public void addEdge(V from, V to, E weight) {
+        // 1. If from , to vertex existing or not
+        Vertex<V, E> fromVertex = vertices.get(from);
+        if (fromVertex == null) {
+            fromVertex = new Vertex<>(from);
+            vertices.put(from, fromVertex);
+        }
+        Vertex<V, E> toVertex = vertices.get(to);
+        if (toVertex == null) {
+            toVertex = new Vertex<>(to);
+            vertices.put(to, toVertex);
+        }
+
 
     }
 
@@ -38,11 +59,19 @@ public class ListGraph<V, E> implements Graph<V, E> {
         V value;
         Set<Edge<V, E>> inEdges = new HashSet<>();
         Set<Edge<V, E>> outEdges = new HashSet<>();
+
+        public Vertex(V value) {
+            this.value = value;
+        }
     }
 
     private static class Edge<V, E> {
         E weight;
         Vertex<V, E> from;
         Vertex<V, E> end;
+
+        public Edge(E weight) {
+            this.weight = weight;
+        }
     }
 }
