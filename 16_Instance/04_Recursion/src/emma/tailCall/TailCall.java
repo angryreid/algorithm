73359a811d5eda6196e8b73fad1efd5f6ff362cf
angryreid <onyourwill@gmail.com>
2023-02-27 21:53:39 +0800

    Code: Tail call samples

--- 16_Instance/04_Recursion/src/emma/tailCall/TailCall.java
+++ 16_Instance/04_Recursion/src/emma/tailCall/TailCall.java
@@ -6,6 +6,32 @@ public class TailCall {
      * Optimization by Compiler
      *
      * Tail function will keep executing in the source function
+     *
+     * Better to implement tail call which can help performance upgrade.
      */
 
+    public static int factorial(int n) {
+        if (n <= 1) return n;
+        return n * factorial(n - 1);
+    }
+
+    public static int factorial(int n, int result) {
+        if (n <= 1) return result;
+        return factorial(n - 1, n * result);
+    }
+
+    public static int fib(int n) {
+        return fib(n, 1, 1);
+    }
+
+    public static int fib(int n, int first, int second) {
+        if (n <= 1) return first;
+        return fib(n - 1, second, first + second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+        System.out.println(factorial(4, 1));
+        System.out.println(fib(5));
+    }
 }
