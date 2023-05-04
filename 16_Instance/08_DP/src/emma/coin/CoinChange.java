347a43c77ed86bd1d9d88497696dbdc54dcd263b
angryreid <onyourwill@gmail.com>
2023-05-04 08:02:49 +0800

    F: DP :coin

--- 16_Instance/08_DP/src/emma/coin/CoinChange.java
+++ 16_Instance/08_DP/src/emma/coin/CoinChange.java
@@ -1,9 +1,10 @@
 package emma.coin;
 
-public class CoinChange {
+ public class CoinChange {
     public static void main(String[] args) {
         int money = 41;
         System.out.println(coins(money));
+        System.out.println(coins2(money));
 
     }
 
@@ -14,4 +15,26 @@ public class CoinChange {
         int min2 = Math.min(coins(n - 5), coins(n - 1));
         return Math.min(min1, min2) + 1;
     }
+     static int coins2(int n) {
+         if (n < 1) return -1;
+         int[] dp = new int[n + 1];
+         int[] faces = {1, 5, 20, 25};
+         for (int face :
+                 faces) {
+             if (n < face) continue;
+             dp[face] = 1;
+         }
+         dp[1] = dp[5] = dp[20] = dp[25] = 1;
+         return coins2(n, dp);
+     }
+
+     static int coins2(int n, int[] dp) {
+         if (n < 1) return Integer.MAX_VALUE;
+         if (dp[n] == 0) {
+             int min1 = Math.min(coins2(n - 25, dp), coins2(n - 20, dp));
+             int min2 = Math.min(coins2(n - 5, dp), coins2(n - 1, dp));
+             dp[n] = Math.min(min1, min2) + 1;
+         }
+         return dp[n];
+     }
 }
