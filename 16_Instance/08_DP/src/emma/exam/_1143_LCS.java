2f613e67ac97a8cfcda84acca460d060bbf9af2f
angryreid <onyourwill@gmail.com>
2023-05-07 16:14:26 +0800

    LC: LCS enhancement

--- 16_Instance/08_DP/src/emma/exam/_1143_LCS.java
+++ 16_Instance/08_DP/src/emma/exam/_1143_LCS.java
@@ -29,8 +29,46 @@ public class _1143_LCS {
         }
         return dp[nums1.length][nums2.length];
     }
+
+    public static int lcs3(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) return 0;
+        int[][] dp = new int[2][nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            int preRow = (i - 1) % 2;
+            int row = i % 2; // i & 1
+            for (int j = 1; j <= nums2.length; j++) {
+                if(nums1[i - 1] == nums2[j - 1]) {
+                    dp[row][j] = dp[preRow][j - 1] + 1;
+                } else {
+                    dp[row][j] = Math.max(dp[row][j - 1], dp[preRow][j]);
+                }
+            }
+        }
+        return dp[nums1.length % 2][nums2.length];
+    }
+
+    public static int lcs4(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) return 0;
+        int[] dp = new int[nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            int cur = 0;
+            for (int j = 1; j <= nums2.length; j++) {
+                int leftTop = cur;
+                cur = dp[j];
+                if(nums1[i - 1] == nums2[j - 1]) {
+                    dp[j] = leftTop + 1;
+                } else {
+                    dp[j] = Math.max(dp[j - 1], dp[j]);
+                }
+            }
+        }
+        return dp[nums2.length];
+    }
+
     public static void main(String[] args) {
         System.out.println(lcs(new int[]{1,3,5,9,10}, new int[]{1,4,9,10}));
         System.out.println(lcs2(new int[]{1,4,5,9,10}, new int[]{1,4,9,10}));
+        System.out.println(lcs3(new int[]{1,2,4,5,9,10}, new int[]{1,4,9,10}));
+        System.out.println(lcs4(new int[]{1,2,4,5,9,10}, new int[]{1,4,9,10}));
     }
 }
