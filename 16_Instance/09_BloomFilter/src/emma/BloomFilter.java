03425c61c7f526e95ed002520481db2d2741a389
angryreid <onyourwill@gmail.com>
2023-05-11 08:33:02 +0800

    F: Bloom Filter bis size

--- 16_Instance/09_BloomFilter/src/emma/BloomFilter.java
+++ 16_Instance/09_BloomFilter/src/emma/BloomFilter.java
@@ -16,6 +16,7 @@ public class BloomFilter<T> {
         double ln2 = Math.log(2);
         bitSize = (int) (- (n * Math.log(p)) / (ln2 * ln2));
         hashSize = (int) (bitSize * ln2 / n);
+        bits = new long[(bitSize + Long.SIZE - 1) / Long.SIZE];
         System.out.println(bitSize + " " + hashSize);
     }
     /**
