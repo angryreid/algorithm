3ac67cbc0fdbbbbdb989a1a6e1ee5fb144c0e400
angryreid <onyourwill@gmail.com>
2023-07-17 22:07:09 +0800

    Bloom Filter Completed

--- 16_Instance/09_BloomFilter/src/emma/BloomFilter.java
+++ 16_Instance/09_BloomFilter/src/emma/BloomFilter.java
@@ -16,7 +16,7 @@ public class BloomFilter<T> {
         double ln2 = Math.log(2);
         bitSize = (int) (- (n * Math.log(p)) / (ln2 * ln2));
         hashSize = (int) (bitSize * ln2 / n);
-        bits = new long[(bitSize + Long.SIZE - 1) / Long.SIZE];
+        bits = new long[(bitSize + Long.SIZE - 1) / Long.SIZE]; // Long.Size = 64
         System.out.println(bitSize + " " + hashSize);
     }
     /**
@@ -24,7 +24,17 @@ public class BloomFilter<T> {
      * @param value
      */
     public void put (T value) {
-
+        nullCheck(value);
+        int hash1 = value.hashCode();
+        int hash2 = hash1 >>> 16;
+        for (int i = 1; i <= hashSize; i++) {
+            int combinedHash = hash1 + (i * hash2);
+            if (combinedHash < 0) {
+                combinedHash = ~combinedHash;
+            }
+            int index = combinedHash % bitSize;
+            set(index);
+        }
     }
 
     /**
@@ -33,6 +43,45 @@ public class BloomFilter<T> {
      * @return true -> existing, false -> not existing
      */
     public boolean contains(T value) {
+        nullCheck(value);
+        int hash1 = value.hashCode();
+        int hash2 = hash1 >>> 16;
+        for (int i = 1; i <= hashSize; i++) {
+            int combinedHash = hash1 + (i * hash2);
+            if (combinedHash < 0) {
+                combinedHash = ~combinedHash;
+            }
+            int index = combinedHash % bitSize;
+            if (!get(index)) return false;
+        }
         return true;
+    }
+
+    private void nullCheck(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Value must not be null");
+        }
+    }
+
+    /**
+     * Set bits index as 1
+     * @param index
+     */
+    private void set(int index) {
+        long chunk = bits[index / Long.SIZE];
+        int pointer = index % Long.SIZE;
+        bits[index / Long.SIZE] = chunk | (1 << pointer);
+    }
+
+    /**
+     * Get bits index value and reture true for 1, false for 0
+     * @param index
+     * @return
+     */
+    private boolean get(int index) {
+        long chunk = bits[index / Long.SIZE];
+        int pointer = index % Long.SIZE;
+        long value = chunk & ( 1 << pointer );
+        return value != 0;
     }
 }
--- 16_Instance/09_BloomFilter/src/emma/Main.java
+++ 16_Instance/09_BloomFilter/src/emma/Main.java
@@ -2,6 +2,22 @@ package emma;
 
 public class Main {
     public static void main(String[] args) {
-        BloomFilter<Integer> bf = new BloomFilter<>(1_0000_0000, 0.01);
+        BloomFilter<Integer> bf = new BloomFilter<>(1_00_0000, 0.01);
+//        for (int i = 0; i < 500; i++) {
+//            bf.put(i);
+//        }
+//        for (int i = 0; i < 500; i++) {
+//            System.out.println(bf.contains(i));
+//        }
+        long count = 0;
+        for (int i = 0; i < 1_00_0000; i++) {
+            bf.put(i);
+        }
+        for (int i = 1_00_0001; i < 2_00_0000; i++) {
+            if(bf.contains(i)) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
