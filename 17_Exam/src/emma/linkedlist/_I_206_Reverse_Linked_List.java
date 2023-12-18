dcc38dfef577d8eb014280478326ae53aa5f9e85
angryreid <onyourwill@gmail.com>
2023-12-18 23:02:17 +0800

    LeetCode

--- 17_Exam/src/emma/linkedlist/_I_206_Reverse_Linked_List.java
+++ 17_Exam/src/emma/linkedlist/_I_206_Reverse_Linked_List.java
@@ -1,4 +1,9 @@
 package emma.linkedlist;
 
 public class _I_206_Reverse_Linked_List {
+  if (head == null || head.next == null) return head;
+    ListNode newHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return newHead;
 }
