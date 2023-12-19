c0a5745079f8d949ee8f17053be132a4c21f6fd7
angryreid <onyourwill@gmail.com>
2023-12-19 21:39:04 +0800

    LeetCode

--- 17_Exam/src/emma/linkedlist/_II_160_Intersection_of_Two_Linked_Lists.java
+++ 17_Exam/src/emma/linkedlist/_II_160_Intersection_of_Two_Linked_Lists.java
@@ -9,7 +9,29 @@ public class _II_160_Intersection_of_Two_Linked_Lists {
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
-    
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+      if (headA == null || headB == null) return null;
+      ListNode travalA = headA;
+      ListNode travalB = headB;
+
+      while (travalA != travalB) {
+          if (travalA != null) {
+              travalA = travalA.next;
+          } else {
+              travalA = headB;
+          }
+
+          if (travalB != null) {
+              travalB = travalB.next;
+          } else {
+              travalB = headA;
+          }
+      }
+
+      return travalA;
+    }
+
     public static void main(String[] args) {
 
     }
