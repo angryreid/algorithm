bfc0c6be9e06d22a4db11c66e6d946d9270d0174
angryreid <onyourwill@gmail.com>
2023-12-19 21:28:38 +0800

    LeetCode

--- 17_Exam/src/emma/linkedlist/_II_2_Add_Two_Numbers.java
+++ 17_Exam/src/emma/linkedlist/_II_2_Add_Two_Numbers.java
@@ -11,7 +11,35 @@ public class _II_2_Add_Two_Numbers {
     }
   
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-      return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int v1 = 0;
+            int v2 = 0;
+
+            if (l1 != null) {
+                v1 = l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                v2 = l2.val;
+                l2 = l2.next;
+            }
+
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            tail.next = new ListNode(sum % 10);
+            tail = tail.next;
+        }
+        if (carry != 0) {
+            tail.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 
     public static void main(String[] args) {
