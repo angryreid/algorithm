31071a2d529deafeed20203a9eabff24043c404c
angryreid <onyourwill@gmail.com>
2023-12-20 20:50:18 +0800

    LC: _II_86_Partition_List

--- 17_Exam/src/emma/linkedlist/_II_86_Partition_List.java
+++ 17_Exam/src/emma/linkedlist/_II_86_Partition_List.java
@@ -2,6 +2,26 @@ package emma.linkedlist;
 
 public class _II_86_Partition_List {
     public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) return head;
+        ListNode left = new ListNode(0);
+        ListNode leftTail = left;
+        ListNode right = new ListNode(0);
+        ListNode rigthTail = right;
+        
+        while (head != null) {
+            if (head.val < x) {
+                leftTail = leftTail.next = head;
+            } else {
+                rigthTail = rigthTail.next = head;
+            }
+            head = head.next;
+        }
 
+        if (rigthTail != null) {
+            rigthTail.next = null;
+        }
+
+        leftTail.next = right.next;
+        return left.next;
     }
 }
