d960218c4fd42498d2063f9d35cfaba3fb23d21c
angryreid <onyourwill@gmail.com>
2023-12-27 21:10:41 +0800

    LeetCode

---
+++ 17_Exam/src/emma/stackqueue/README.md
@@ -0,0 +1,32 @@
+# Stack and Queue
+
+## Pre-requisites
+
+1. Basic operation of Stack and Queue
+2. How to implement Stack and Queue using LinkedList
+3. How to implement Stack and Queue using Array
+4. How to implement Stack and Queue using Deque
+5. How to implement Stack and Queue using Priority Queue
+6. How to implement Stack and Queue using Heap
+7. How to implement Stack and Queue using HashMap
+8. How to implement Stack and Queue using HashSet
+9. How to implement Stack and Queue using Stack
+10. How to implement Stack and Queue using Queue
+11. How to implement Stack and Queue using ArrayDeque
+12. How to implement Stack and Queue using LinkedList
+
+## Exams
+
+<!-- create markdown tabble with following columns -->
+
+<!-- 1. Exam Solution Class
+1. Exam Name
+2. Exam Link
+3. Exam Difficulty -->
+
+<!-- Note to add prefix _I_ or _II_ or _III_ for exam solution class name III means hard, II means medium, I means easy-->
+
+| Exam Solution Class| Exam Name | Exam Link | Exam Difficulty |
+| --- | --- | --- | --- |
+<!-- 155 -->
+| `_I_155_Min_Stack` | Min Stack | https://leetcode.com/problems/min-stack/ | Easy |
---
+++ 17_Exam/src/emma/stackqueue/_I_155_Min_Stack.java
@@ -0,0 +1,40 @@
+package emma.stackqueue; // Define the package
+
+import java.util.Stack;
+
+public class _I_155_Min_Stack { // Define the class
+  private Stack<Integer> stack;
+  private Stack<Integer> minStack;
+
+  public _I_155_Min_Stack() { // Define the constructor
+    stack = new Stack<>();
+    minStack = new Stack<>();
+    minStack.push(Integer.MAX_VALUE);
+  }
+
+  public void push(int val) { // Define a method to push a value onto the stack
+    stack.push(val);
+    minStack.push(Math.min(val, minStack.peek()));
+  }
+
+  public void pop() { // Define a method to remove the top element from the stack
+    if (!stack.isEmpty()) {
+      stack.pop();
+      minStack.pop();
+    }
+  }
+
+  public int top() { // Define a method to get the top element of the stack
+    if (!stack.isEmpty()) {
+      return stack.peek();
+    }
+    return -1; // Return -1 or any other value indicating an error
+  }
+
+  public int getMin() { // Define a method to get the minimum element in the stack
+    if (!minStack.isEmpty()) {
+      return minStack.peek();
+    }
+    return -1; // Return -1 or any other value indicating an error
+  }
+}
