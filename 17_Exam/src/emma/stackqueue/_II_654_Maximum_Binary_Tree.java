6e0ab7c5d0a30a7792fea5115cd0fb24e8eb5db5
angryreid <onyourwill@gmail.com>
2023-12-29 09:41:09 +0800

    LeetCode

--- 17_Exam/src/emma/stackqueue/_II_654_Maximum_Binary_Tree.java
+++ 17_Exam/src/emma/stackqueue/_II_654_Maximum_Binary_Tree.java
@@ -29,6 +29,44 @@ public class _II_654_Maximum_Binary_Tree {
         return root;
     }
 
+    public int[] parentIndexes(int[] nums) {
+        /**
+         * 1. Traverse the array
+         * 2. Keep stack of indexes of elements that are smaller than the current element
+         * 3. If the stack is empty, the parent of the current element is -1
+         */
+        int len = nums.length;
+        int[] lis = new int[len];
+        int[] ris = new int[len];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            ris[i] = -1;
+            lis[i] = -1;
+            int v = nums[i];
+            while (!stack.isEmpty() && v >= nums[stack.peek()]) {
+                ris[stack.pop()] = i;
+            }
+            if (!stack.isEmpty()) {
+                lis[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+
+        int[] parents = new int[len];
+        for (int i = 0; i < len; i++) {
+            if (lis[i] == -1 && ris[i] == -1) {
+                parents[i] = -1;
+            } else if (lis[i] == -1) {
+                parents[i] = ris[i];
+            } else if (ris[i] == -1) {
+                parents[i] = lis[i];
+            } else {
+                parents[i] = nums[lis[i]] > nums[ris[i]] ? lis[i] : ris[i];
+            }
+        }
+        return parents;
+    }
+
     /**
      * Definition for a binary tree node.
      */
