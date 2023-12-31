e9fec98b78a816d2af6cbe1c8e821155835b9cab
angryreid <onyourwill@gmail.com>
2023-12-31 15:58:37 +0800

    LC | Daily Temperature | Enhance stack to DP

--- 17_Exam/src/emma/stackqueue/_II_739_Daily_Temperatures.java
+++ 17_Exam/src/emma/stackqueue/_II_739_Daily_Temperatures.java
@@ -3,7 +3,54 @@ package emma.stackqueue;
 import java.util.Stack;
 
 public class _II_739_Daily_Temperatures {
+
+    public int[] dailyTemperatures0(int[] temperatures) {
+        int len = temperatures.length;
+        int[] values = new int[len];
+        for (int i = len - 2; i >= 0; i--) {
+            int j = i + 1;
+            while (true) {
+                if (temperatures[j] > temperatures[i]) {
+                    values[i] = j - i;
+                    break;
+                } else if (values[j] == 0) {
+                    values[i] = 0;
+                    break;
+                } else {
+                    j += values[j];
+                }
+            }
+        }
+        return values;
+    }
+
     public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] values = new int[len];
+        for (int i = len - 2; i >= 0; i--) {
+            int j = i + 1;
+            while (true) {
+                if (temperatures[j] > temperatures[i]) {
+                    values[i] = j - i;
+                    break;
+                } else {
+                    if (values[j] == 0) {
+                        values[i] = 0;
+                        break;
+                    }
+                    if (temperatures[i] == temperatures[j]) {
+                        values[i] = values[j] + j - i;
+                        break;
+                    } else {
+                        j += values[j];
+                    }
+                }
+            }
+        }
+        return values;
+    }
+
+    public int[] dailyTemperatures1(int[] temperatures) {
         int len = temperatures.length;
         int[] nextHighTmp = new int[len];
         Stack<Integer> stack = new Stack();
