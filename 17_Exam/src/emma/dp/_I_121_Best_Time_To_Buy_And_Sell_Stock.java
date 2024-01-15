fb97350a5f2366c77e721e24c5f39bdcfa8024ef
angryreid <onyourwill@gmail.com>
2024-01-15 20:15:34 +0800

    LC | DP | _I_121_Best_Time_To_Buy_And_Sell_Stock | Java more solutions

--- 17_Exam/src/emma/dp/_I_121_Best_Time_To_Buy_And_Sell_Stock.java
+++ 17_Exam/src/emma/dp/_I_121_Best_Time_To_Buy_And_Sell_Stock.java
@@ -13,7 +13,50 @@ public class _I_121_Best_Time_To_Buy_And_Sell_Stock {
                 maxProfit = Math.max(maxProfit, prices[i] - minPrice);
             }
         }
-        return 0;
+        return maxProfit;
+    }
+
+    public int maxProfit2(int[] prices) {
+        int len = prices.length;
+        if (len == 1) return 0;
+        int[] profits = new int[len - 1];
+        for (int i = 0; i < len - 1; i++) {
+            profits[i] = prices[i + 1] - prices[i];
+        }
+        int[] dp = new int[len - 1];
+        dp[0] = profits[0];
+        int max = Math.max(0, dp[0]);
+        for (int i = 1; i < len - 1; i++) {
+            int prev = dp[i - 1];
+            if (prev > 0) {
+                dp[i] = prev + profits[i];
+            } else {
+                dp[i] = profits[i];
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public int maxProfit3(int[] prices) {
+        int len = prices.length;
+        if (len == 1) return 0;
+        int[] profits = new int[len - 1];
+        for (int i = 0; i < len - 1; i++) {
+            profits[i] = prices[i + 1] - prices[i];
+        }
+        int dp = profits[0];
+        int max = Math.max(0, dp);
+        for (int i = 1; i < len - 1; i++) {
+            int prev = dp;
+            if (prev > 0) {
+                dp = prev + profits[i];
+            } else {
+                dp = profits[i];
+            }
+            max = Math.max(max, dp);
+        }
+        return max;
     }
 
 //    var maxProfit = function(prices) {
