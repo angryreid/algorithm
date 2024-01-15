0bcd06c2196b234f0f13c4d0c5bfa5c6a75312f1
angryreid <onyourwill@gmail.com>
2024-01-15 19:47:32 +0800

    LC | DP | _I_121_Best_Time_To_Buy_And_Sell_Stock | Java

--- 17_Exam/src/emma/dp/_I_121_Best_Time_To_Buy_And_Sell_Stock.java
+++ 17_Exam/src/emma/dp/_I_121_Best_Time_To_Buy_And_Sell_Stock.java
@@ -2,6 +2,17 @@ package emma.dp;
 
 public class _I_121_Best_Time_To_Buy_And_Sell_Stock {
     public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len == 1) return 0;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < len; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
         return 0;
     }
 
