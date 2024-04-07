aa1b2aa11c1c4f9067b13a27184fe009820a1f00
angryreid <onyourwill@gmail.com>
2024-04-07 10:37:47 +0800

    Refactor lowestCommonAncestor method to handle edge cases and find p or q in different subtrees

--- 17_Exam/src/emma/tree/_II_236_Lowest_Common_Ancestor_Of_A_Binary_Tree.java
+++ 17_Exam/src/emma/tree/_II_236_Lowest_Common_Ancestor_Of_A_Binary_Tree.java
@@ -11,8 +11,10 @@ public class _II_236_Lowest_Common_Ancestor_Of_A_Binary_Tree {
      * @return the lowest common ancestor of p and q
      */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // handle edge cases
-        if (root == null || root == p || root == q) return root;
-        return null;
+        if (root == null || root == p || root == q) return root; // handle edge cases
+        TreeNode left = lowestCommonAncestor(root.left, p, q); // find p or q in left subtree
+        TreeNode right = lowestCommonAncestor(root.right, p, q); // find p or q in right subtree
+        if (left != null && right != null) return root; // p and q are in different subtrees
+        return (left != null) ? left : right; // p and q are in the same subtree
     }
 }
