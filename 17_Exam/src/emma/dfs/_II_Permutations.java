035a344cdedc31d10d3ba44f1775177496911a9d
angryreid <onyourwill@gmail.com>
2024-04-14 21:20:06 +0800

    Refactor _II_Permutations class to use a boolean array to track used numbers in dfs method

--- 17_Exam/src/emma/dfs/_II_Permutations.java
+++ 17_Exam/src/emma/dfs/_II_Permutations.java
@@ -10,19 +10,22 @@ public class _II_Permutations {
         List<List<Integer>> list = new ArrayList<>();
         if (len == 0) return list;
         List<Integer> res = new ArrayList<>();
-        dfs(list, res, nums);
+        boolean[] used = new boolean[len]; // Boolean array to keep track of used numbers
+        dfs(list, res, nums, used);
         return list;
     }
 
-    private void dfs(List<List<Integer>> list, List<Integer> res, int[] nums) {
+    private void dfs(List<List<Integer>> list, List<Integer> res, int[] nums, boolean[] used) {
         if (res.size() == nums.length) {
             list.add(new ArrayList<>(res));
             return;
         }
-        for (int num : nums) {
-            if (res.contains(num)) continue; // Skip the number if it's already in res
-            res.add(num);
-            dfs(list, res, nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue; // Skip the number if it's already in res
+            res.add(nums[i]);
+            used[i] = true; // Mark the number as used
+            dfs(list, res, nums, used);
+            used[i] = false; // Unmark the number after the recursive call
             res.remove(res.size() - 1); // Remove the last added number
         }
     }
