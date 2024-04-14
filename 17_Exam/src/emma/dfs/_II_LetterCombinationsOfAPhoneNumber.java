9593c827e87adbeaf6d3b731a62f5c74d6c6a035
angryreid <onyourwill@gmail.com>
2024-04-14 20:11:17 +0800

    Add letter combinations of a phone number algorithm

--- 17_Exam/src/emma/dfs/_II_LetterCombinationsOfAPhoneNumber.java
+++ 17_Exam/src/emma/dfs/_II_LetterCombinationsOfAPhoneNumber.java
@@ -1,9 +1,38 @@
 package emma.dfs;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class _II_LetterCombinationsOfAPhoneNumber {
-    public List<String> letterCombinations(String digits) {
 
+    private List<String> res;
+    private char[] chars;
+    private final String[] lettersList = new String[] {
+            "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    }; // 2-9
+
+    private char[] string;
+    public List<String> letterCombinations(String digits) {
+        if (digits == null) return null;
+        res = new ArrayList<>();
+        chars = digits.toCharArray();
+        int len = chars.length;
+        if (len == 0) return res;
+        string = new char[len];
+        dfs(0);
+        return res;
+    }
+
+    private void dfs(int idx) {
+        if (idx == chars.length) {
+            // add value;
+            res.add(new String(string));
+            return;
+        }
+        char[] letters = lettersList[chars[idx] - '2'].toCharArray();
+        for (char ltr : letters) {
+            string[idx] = ltr;
+            dfs(idx + 1);
+        }
     }
 }
