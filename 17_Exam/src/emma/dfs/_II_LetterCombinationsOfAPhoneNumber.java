b5a82d3fe5dd0c9dbd8717638b89b331dab3d71e
angryreid <onyourwill@gmail.com>
2024-04-14 20:42:32 +0800

    Refactor letterCombinations method in _II_LetterCombinationsOfAPhoneNumber.java

--- 17_Exam/src/emma/dfs/_II_LetterCombinationsOfAPhoneNumber.java
+++ 17_Exam/src/emma/dfs/_II_LetterCombinationsOfAPhoneNumber.java
@@ -4,26 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _II_LetterCombinationsOfAPhoneNumber {
-
-    private List<String> res;
-    private char[] chars;
     private final String[] lettersList = new String[] {
             "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
     }; // 2-9
-
-    private char[] string;
     public List<String> letterCombinations(String digits) {
         if (digits == null) return null;
-        res = new ArrayList<>();
-        chars = digits.toCharArray();
+        List<String> res = new ArrayList<>();
+        char[] chars = digits.toCharArray();
         int len = chars.length;
         if (len == 0) return res;
-        string = new char[len];
-        dfs(0);
+        char[] string = new char[len];
+        dfs(0, res, chars, string);
         return res;
     }
 
-    private void dfs(int idx) {
+    private void dfs(int idx, List<String> res, char[] chars, char[] string) {
         if (idx == chars.length) {
             // add value;
             res.add(new String(string));
@@ -32,7 +27,7 @@ public class _II_LetterCombinationsOfAPhoneNumber {
         char[] letters = lettersList[chars[idx] - '2'].toCharArray();
         for (char ltr : letters) {
             string[idx] = ltr;
-            dfs(idx + 1);
+            dfs(idx + 1, res, chars, string);
         }
     }
 }
