6fecacb18cedbd859156266fea8fd47fcde9d902
angryreid <onyourwill@gmail.com>
2024-04-16 22:18:48 +0800

    Refactor _II_GenerateParentheses class to use a DFS algorithm for generating parentheses

--- 17_Exam/src/emma/dfs/_II_GenerateParentheses.java
+++ 17_Exam/src/emma/dfs/_II_GenerateParentheses.java
@@ -24,3 +24,28 @@ public class _II_GenerateParentheses {
         }
     }
 }
+
+/**
+ *
+ * Solution 1
+ *
+ * List<String> res;
+ *     public List<String> generateParenthesis(int n) {
+ *         res = new ArrayList<>();
+ *         dfs(n, n, "");
+ *         return res;
+ *     }
+ *
+ *     private void dfs(int l, int r, String s) {
+ *         if (l == 0 && r == 0) {
+ *             res.add(s);
+ *             return;
+ *         }
+ *         if (l > 0) {
+ *             dfs(l - 1, r, s + "(");
+ *         }
+ *         if (l < r) {
+ *             dfs(l, r - 1, s + ")");
+ *         }
+ *     }
+ */
