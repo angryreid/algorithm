2445d1a0c291440b6aa6606b6765df4352df1627
angryreid <onyourwill@gmail.com>
2024-04-23 22:57:22 +0800

    Refactor _II_SpiralMatrix class to improve code readability and add additional spiralOrder3 method

--- 00_Leetcode/src/_06_math/_II_SpiralMatrix.java
+++ 00_Leetcode/src/_06_math/_II_SpiralMatrix.java
@@ -35,42 +35,77 @@ public class _II_SpiralMatrix {
             left++;
         }
         return res;
-
-        // if (matrix == null) return null;
-        // List<Integer> res = new ArrayList<>();
-        // int len = matrix.length;
-        // if (len == 0) return res;
-
-        // int top = 0, bottom = len - 1;
-        // int left = 0, right = matrix[0].length - 1;
-        // while (top <= bottom && left <= right) {
-        //     // left top -> right top
-        //     for (int i = left; i <= right; i++) {
-        //         res.add(matrix[top][i]);
-        //     }
-        //     top++;
-        //     // right top -> right bottom
-        //     for (int i = top; i <= bottom ; i++) {
-        //         res.add(matrix[i][right]);
-        //     }
-        //     right--;
-        //     // right bottom -> left bottom
-        //     if (top <= bottom) {
-        //         for (int i = right; i >= left ; i--) {
-        //             res.add(matrix[bottom][i]);
-        //         }
-        //     }
-        //     bottom--;
-        //     // left bottom -> left top
-        //     if (left <= right) {
-        //         for (int i = bottom; i >= top; i--) {
-        //             res.add(matrix[i][left]);
-        //         }
-        //     }
-        //     left++;
-        // }
-        // return res;
     }
 
 
+    public List<Integer> spiralOrder2(int[][] matrix) {
+         if (matrix == null) return null;
+         List<Integer> res = new ArrayList<>();
+         int len = matrix.length;
+         if (len == 0) return res;
+
+         int top = 0, bottom = len - 1;
+         int left = 0, right = matrix[0].length - 1;
+         while (top <= bottom && left <= right) {
+             // left top -> right top
+             for (int i = left; i <= right; i++) {
+                 res.add(matrix[top][i]);
+             }
+             top++;
+             // right top -> right bottom
+             for (int i = top; i <= bottom ; i++) {
+                 res.add(matrix[i][right]);
+             }
+             right--;
+             // right bottom -> left bottom
+             if (top <= bottom) {
+                 for (int i = right; i >= left ; i--) {
+                     res.add(matrix[bottom][i]);
+                 }
+             }
+             bottom--;
+             // left bottom -> left top
+             if (left <= right) {
+                 for (int i = bottom; i >= top; i--) {
+                     res.add(matrix[i][left]);
+                 }
+             }
+             left++;
+         }
+         return res;
+    }
+
+    public List<Integer> spiralOrder3(int[][] matrix) {
+        if (matrix == null) return null;
+        List<Integer> res = new ArrayList<>();
+        int len = matrix.length;
+        if (len == 0) return res;
+
+        int top = 0, bottom = len - 1;
+        int left = 0, right = matrix[0].length - 1;
+        while (top <= bottom && left <= right) {
+            // left top -> right top
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom ; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top > bottom || left > right) break;
+
+            for (int i = right; i >= left ; i--) {
+                res.add(matrix[bottom][i]);
+            }
+            bottom--;
+
+            for (int i = bottom; i >= top; i--) {
+                res.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return res;
+    }
 }
