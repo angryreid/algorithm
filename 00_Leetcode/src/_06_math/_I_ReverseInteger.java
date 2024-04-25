f040374328ac04b2f5dcb45c7b6a2b1809ac540b
angryreid <onyourwill@gmail.com>
2024-04-25 22:13:08 +0800

    LC: 7 Basic case

--- 00_Leetcode/src/_06_math/_I_ReverseInteger.java
+++ 00_Leetcode/src/_06_math/_I_ReverseInteger.java
@@ -7,14 +7,11 @@ public class _I_ReverseInteger {
      * @return
      */
     public int reverse(int x) {
-        boolean negtive = x < 0;
-        x = negtive ? -x : x;
         int res = 0;
         while (x != 0) {
-            int tail = x % 10;
-            res = res * 10 + tail;
+            res = res * 10 +  x % 10;
             x = x / 10;
         }
-        return negtive ? -res : res;
+        return res;
     }
 }
