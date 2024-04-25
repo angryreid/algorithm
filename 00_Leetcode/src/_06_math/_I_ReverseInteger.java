99522f111615eb2a1f5cc51c3748944f3ee28050
angryreid <onyourwill@gmail.com>
2024-04-25 22:08:25 +0800

    Add reverse method to _I_ReverseInteger.java for integer reversal

--- 00_Leetcode/src/_06_math/_I_ReverseInteger.java
+++ 00_Leetcode/src/_06_math/_I_ReverseInteger.java
@@ -7,6 +7,14 @@ public class _I_ReverseInteger {
      * @return
      */
     public int reverse(int x) {
-
+        boolean negtive = x < 0;
+        x = negtive ? -x : x;
+        int res = 0;
+        while (x != 0) {
+            int tail = x % 10;
+            res = res * 10 + tail;
+            x = x / 10;
+        }
+        return negtive ? -res : res;
     }
 }
