dcca0ce22ffa5c0c00a8271b6eab3278e4e820a5
angryreid <onyourwill@gmail.com>
2024-04-25 22:16:28 +0800

    LC: 7 Basic case fixed

--- 00_Leetcode/src/_06_math/_I_ReverseInteger.java
+++ 00_Leetcode/src/_06_math/_I_ReverseInteger.java
@@ -14,4 +14,14 @@ public class _I_ReverseInteger {
         }
         return res;
     }
+
+    public int reverse2(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 +  x % 10;
+            if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) return 0;
+            x = x / 10;
+        }
+        return (int) res;
+    }
 }
