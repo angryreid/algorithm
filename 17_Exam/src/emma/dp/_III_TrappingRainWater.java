b7f24f8c22389d8ddc2ed76041336b44d05eeb22
angryreid <onyourwill@gmail.com>
2024-05-02 21:42:34 +0800

    Add maxArea2 method to _III_TrappingRainWater.java

--- 17_Exam/src/emma/dp/_III_TrappingRainWater.java
+++ 17_Exam/src/emma/dp/_III_TrappingRainWater.java
@@ -25,4 +25,26 @@ public class _III_TrappingRainWater {
         }
         return res;
     }
+    public int maxArea2(int[] height) {
+        if (height == null)
+            return 0;
+        int len = height.length;
+        if (len <= 2)
+            return 0;
+        int[] rightMax = new int[len];
+        int res = 0;
+        for (int i = len - 2; i >= 1; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i + 1]);
+        }
+
+        int leftMax = 0;
+        for (int i = 1; i < len - 1; i++) {
+            leftMax = Math.max(leftMax, height[i - 1]);
+            int min = Math.min(leftMax[i], rightMax[i]);
+            if (min <= height[i])
+                continue;
+            res += min - height[i];
+        }
+        return res;
+    }
 }
